9f434059faf7215407e8dfbe898c75fbf7207561
Hemanthborra143 <borrahemanth2003@gmail.com>
2024-11-17 20:23:16 +0530

    Add files via upload

---
+++ Day 2/D.java
@@ -0,0 +1,15 @@
+class D
+{
+   public static void main(String []args)
+ {
+   char ch='e';
+   if(ch='a' ||ch='e' ||ch='j')
+   {
+    System.out.println("ovel");
+   }
+  else
+  {
+   System.out.println("constsnts");
+  }
+}
+}
---
+++ Day 2/F.java
@@ -0,0 +1,29 @@
+class F
+{
+public static void main(String args[])
+{
+int a=60,b=30,c=20;
+if(a>b)
+{
+  if(a>c)
+  {
+  System.out.println("a is big");
+  }
+  else
+  {
+  System.out.println("b is big");
+  }
+}
+else
+ {
+   if(b>c)
+    {
+     System.out.println("b is big");
+    }
+   else
+   {
+    System.out.println("c is big");
+   }
+ }
+}
+}
